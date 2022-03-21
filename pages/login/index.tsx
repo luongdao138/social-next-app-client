493d81690bc3419f396014e43b1ce4dd816b0306
Dao Luong <daovanluongpdl@gmail.com>
2022-03-21 09:13:14 +0700

    add authentication layout

---
+++ pages/login/index.tsx
@@ -0,0 +1,11 @@
+import PageWithLayout from 'constants/page';
+import AuthenticationLayout from 'layouts/AuthenticationLayout';
+import React from 'react';
+
+const LoginPage: PageWithLayout = () => {
+  return <div>LoginPage</div>;
+};
+
+LoginPage.Layout = AuthenticationLayout;
+
+export default LoginPage;
