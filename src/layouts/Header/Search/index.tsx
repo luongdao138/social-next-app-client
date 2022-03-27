51ec64290f19a0445927d3a8a761969ad56ca710
Dao Van Luong <daovanluongpdl@gmail.com>
2022-03-27 09:20:22 +0700

    fix input search

--- src/layouts/Header/Search/index.tsx
+++ src/layouts/Header/Search/index.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
-import { MdSearch } from 'react-icons/md';
+import React, { useRef, useState } from 'react';
+import { MdSearch, MdClose } from 'react-icons/md';
 
-const Search = () => {
+interface Props {
+  user_id: string;
+  onSearch: (keyword: string) => void;
+}
+
+const Search: React.FC<Props> = ({ onSearch, user_id }) => {
+  const [value, setValue] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleRemoveSearch = () => {
+    setValue('');
+    inputRef.current?.focus();
+  };
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!value) return;
+
+    onSearch(value);
+  };
+
   return (
     <div className='relative'>
-      <form>
-        <input type='text' className='w-full outline-none py-1 px-2 rounded border border-solid ' />
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          className='w-full outline-none py-1 px-2 rounded border border-solid '
+          value={value}
+          onChange={handleChange}
+          ref={inputRef}
+        />
       </form>
-      <div className='absolute flex items-center gap-1 opacity-30 top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 pointer-events-none'>
-        <MdSearch />
-        <span className='text-xs'>Enter to search</span>
-      </div>
+      {!value && (
+        <div className='absolute flex items-center gap-1 opacity-30 top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 pointer-events-none'>
+          <MdSearch />
+          <span className='text-xs'>Enter to search</span>
+        </div>
+      )}
+
+      {!!value ? (
+        <span
+          className='absolute top-2/4 -translate-y-2/4 right-2 cursor-pointer text-gray-700'
+          onClick={handleRemoveSearch}
+        >
+          <MdClose />
+        </span>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,16 +1,25 @@
 import { LSRoutes } from 'constants/route.constant';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import { getUserAuth } from 'store/auth/selectors';
+import { useAppSelector } from 'store/hooks';
 import useElementSize from 'utils/hooks/useElementSize';
 import Nav from './Nav';
 import Search from './Search';
+import _ from 'lodash';
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   const [headerRef, { width, height }] = useElementSize();
+  const userAuth = useAppSelector(getUserAuth);
+  const user_id = _.get(userAuth, '_id', '');
 
   const handleToggle = () => {
     setOpenMenu((prev) => !prev);
+  };
+
+  const handleSearch = (keyword: string) => {
+    console.log(keyword);
   };
 
   return (
@@ -24,7 +33,7 @@ const Header = () => {
             <Link href={LSRoutes.HOME}>L-Network</Link>
           </h1>
         </div>
-        <Search />
+        <Search user_id={user_id} onSearch={handleSearch} />
         <Nav open={openMenu} toggle={handleToggle} />
       </div>
       <div style={{ marginBottom: height }}></div>
